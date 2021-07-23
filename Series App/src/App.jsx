ac6599271c56a510d0aa6f85c564bb8f23ec03f5
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-07-23 23:06:38 +0530

    Map function

---
+++ Series App/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Condition from "./condition";
+
+function App(){
+    return (
+        <>
+            <Condition/>
+        </>
+    );
+}
+
+export default App;
