7d8db4d27e46e2cb59b14c9d08758b491a2ff0df
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-07-27 14:22:39 +0530

    Complex input form

--- exp/src/App.jsx
+++ exp/src/App.jsx
@@ -2,35 +2,47 @@ import React, { useState } from "react";
 
 const App=()=>{
 
-    const [name,setname]=useState("");
-    const [name1,setname1]=useState("");
-
-    const [fullname,setfullname]=useState("");
-    const [fullname2,setfullname2]=useState("");
-
-    const anything=(e)=>{
-        console.log(e.target.value);
-        setname(e.target.value);
-    };
-
-    const anything1=(e1)=>{
-        setname1(e1.target.value);
-    };
+    const [fullname,setfullname]=useState({
+        fname: "",
+        lname: ""
+    });
 
     const onSubmitss=(e)=>{
         e.preventDefault();
-        setfullname(name);
-        setfullname2(name1);
+        alert("form submitted");
+    }
+
+    const inputEvent = (e)=>{
+        const value=e.target.value;
+        const name=e.target.name;
+        console.log(value);
+        console.log(name);
+
+        setfullname((preValue)=>{
+            // console.log(preValue);
+            if(name==="fname"){
+                return {
+                    fname : value,
+                    lname : preValue.lname,
+                } 
+            }else{
+                return {
+                    fname: preValue.fname,
+                    lname: value,
+                }
+            }
+        });
+        // setname(e.target.value);
     }
 
     return (
         <>
             <div>
                 <form onSubmit={onSubmitss}>
-                    <h1>Hello {fullname} {fullname2}</h1>
-                    <input type="text" placeholder="Enter your name" onChange={anything} value={name}/>
+                    <h1>Hello {fullname.fname} {fullname.lname}</h1>
+                    <input type="text" placeholder="Enter your name" name="fname" onChange={inputEvent} value={fullname.fname}/>
                     <br/>
-                    <input type="text" placeholder="Enter your password" onChange={anything1} value={name1}/>
+                    <input type="text" placeholder="Enter your password" name="lname" onChange={inputEvent} value={fullname.lname}/>
                     <button type="submit">Click Me 😃</button>
                 </form>
             </div>
