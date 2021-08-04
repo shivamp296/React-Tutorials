12fa118fedb4da457c0b69a9ccabb93cd261c6d1
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-08-04 23:53:31 +0530

    Minor fixes

---
+++ Accordion Web App/src/component/accordion/Accordion.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {questions} from "./api";
+import "./accordion.css";
+
+const Accordion=()=>{
+    return (
+        <>
+            <h1>Thapa</h1>
+        </>
+    );
+}
+
+export default Accordion;
